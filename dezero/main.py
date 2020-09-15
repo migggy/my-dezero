e0809c2238b93807f2986ca3ed10cbae7277c76f
Naoto MIGITA <migggy@users.noreply.github.com>
2020-09-15 11:15:49 +0900

    chapter 19

--- dezero/main.py
+++ dezero/main.py
@@ -5,15 +5,41 @@ import numpy as np
 
 
 class Variable:
-    def __init__(self, data):
+    def __init__(self, data, name=None):
         if data is not None:
             if not isinstance(data, np.ndarray):
                 raise TypeError('{} is not supported'.format(type(data)))
 
         self.data = data
+        self.name = name
         self.grad = None
         self.creator = None
         self.generation = 0
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        if self.data is None:
+            return 'variable(None)'
+        p = str(self.data).replace('\n', '\n' + ' ' * 9)
+        return f'variable({p})'
+
+    @property
+    def shape(self):
+        return self.data.shape
+
+    @property
+    def ndim(self):
+        return self.data.ndim
+
+    @property
+    def size(self):
+        return self.data.size
+
+    @property
+    def dtype(self):
+        return self.data.dtype
 
     def set_creator(self, func):
         self.creator = func
@@ -135,11 +161,9 @@ def no_grad():
 
 
 if __name__ == "__main__":
-    Config.enable_backprop = True
-    x = Variable(np.ones((100, 100, 100)))
-    y = square(square(square(x)))
-    y.backward()
-
-    with no_grad():
-        x = Variable(np.array(2.0))
-        y = square(x)
+    x = Variable(np.array([1, 2, 3]))
+    print(x)
+    x = Variable(None)
+    print(x)
+    x = Variable(np.array([[1, 2, 3], [4, 5, 6]]))
+    print(x)
